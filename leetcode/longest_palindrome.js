02c2fb561b278ab4197a49d2f5ba050adabc0bf8
edwardyi <yi.wei.jie@hotmail.com>
2019-01-14 18:04:04 +0800

    [fixed] condition

--- leetcode/longest_palindrome.js
+++ leetcode/longest_palindrome.js
@@ -1,30 +1,25 @@
 var longestPalindrome = function(s) {
-  if(s.length < 0) {
+  if(s.length <= 1) {
     return s;
   }
   let table = [];
-  // 把回文字串放到table中
-  for(let i= 0; i < s.length;i++) {
-    for(let j=0;j<s.length;j++) {
+  // 把回文字串放到table中(Time Limit Exceeded)
+  for(let i= 0; i <= s.length;i++) {
+    for(let j=0; j <= s.length;j++) {
       var str = s.substring(i,j);
       if (str.length > 0 && str == str.split("").reverse().join("")) {
         table.push(str);
       }
     }
   }
-
-  // 找出最長的回文字串
-  // let result = table[0];
-  // for(let i=0; i<table.length; i++) {
-  //   if(table[i]) {
-  //     result = table[i];
-  //     return true;
-  //   }
-  // }
-  console.log(table, table.concat().sort());
   // console.log('test',table.sort(function(a,b) {return b.length - a.length}));
   return table.sort(function(a,b) {return b.length - a.length})[0];
 };
 
+
+console.log(longestPalindrome("bbb")=="bbb")
+console.log(longestPalindrome("bb")=="bb");
+console.log(longestPalindrome("a")=="a");
+console.log(longestPalindrome("")=="");
 console.log(longestPalindrome("babad") == "bab");
 console.log(longestPalindrome("cbbd") == "bb");
